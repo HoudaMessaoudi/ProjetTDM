e89aa443fa25f37c6631d96c0923dc94c0b6ae3c
HoudaMessaoudi <46978076+HoudaMessaoudi@users.noreply.github.com>
2021-07-03 23:56:29 +0200

    third commit

---
+++ app/src/main/java/com/example/projettdm/Data/Medecin.kt
@@ -0,0 +1,5 @@
+package com.example.projettdm.Data
+
+data class Medecin(val img:Int,val nom:String,val prenom:String, val num:Int, val Specialité:String) {
+
+}
---
+++ app/src/main/java/com/example/projettdm/Data/Traitement.kt
@@ -0,0 +1,4 @@
+package com.example.projettdm.Data
+
+data class Traitement {
+}
