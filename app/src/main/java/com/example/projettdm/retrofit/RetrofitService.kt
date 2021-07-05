9a4bc8c5b462ccc16b8b536b473142e309ed5f8e
HoudaMessaoudi <46978076+HoudaMessaoudi@users.noreply.github.com>
2021-07-05 04:36:23 +0200

    changes on layouts

--- app/src/main/java/com/example/projettdm/retrofit/RetrofitService.kt
+++ app/src/main/java/com/example/projettdm/retrofit/RetrofitService.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitService {
 
     val endpoint : Endpoint by lazy {
-        Retrofit.Builder().baseUrl("http://59baff666a45.ngrok.io/")
+        Retrofit.Builder().baseUrl("http://bc00d7120e03.ngrok.io/")
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(Endpoint::class.java)
     }
