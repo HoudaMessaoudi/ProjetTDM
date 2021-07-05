5064ce5ca02fcde6234bf3dc3a45a188702c7c90
HoudaMessaoudi <46978076+HoudaMessaoudi@users.noreply.github.com>
2021-07-05 22:47:29 +0200

    mlem

--- app/src/main/java/com/example/projettdm/data/RandezVous.kt
+++ app/src/main/java/com/example/projettdm/data/RandezVous.kt
@@ -1,4 +1,4 @@
 package com.example.projettdm.data
-
-data class RandezVous (var id_medecin:String,var nom_medecin:String,var prenom_medecin:String,var id_client:String,var date:String){
+import java.io.Serializable
+data class RandezVous (var id_medecin:String,var nom_medecin:String,var prenom_medecin:String,var id_client:String,var date:String):Serializable{
 }
