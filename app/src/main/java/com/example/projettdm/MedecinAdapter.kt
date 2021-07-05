500d7dc090ba96c7f5f692b9024a81f72a287173
Frog <ha_rezgui@esi.dz>
2021-07-05 23:14:10 +0100

    Added itiniraire and image retrieval

--- app/src/main/java/com/example/projettdm/MedecinAdapter.kt
+++ app/src/main/java/com/example/projettdm/MedecinAdapter.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.projettdm.Activity_test
 import com.example.projettdm.R
 import com.example.projettdm.data.Medecin
@@ -28,7 +29,7 @@ class MyAdapter(val context: Context, var data:List<Medecin>): RecyclerView.Adap
         holder.prenom.text = data[position].prenom
         holder.num.text = data[position].num.toString()
         holder.spec.text = data[position].speciality
-        //Glide.with(context).load("http://ea8f8823bbb4.ngrok.io/"+data[position].image).into(holder.img)
+        Glide.with(context).load("http://eb5796a19dbf.ngrok.io/"+data[position].picture).into(holder.img)
         //holder.img.setImageResource(data[position].img)
         holder.itemView.setOnClickListener{
             val intent = Intent(context, medecinActivity::class.java)
@@ -43,9 +44,9 @@ class MyAdapter(val context: Context, var data:List<Medecin>): RecyclerView.Adap
                 context.startActivity(intent)
             }
             holder.map.setOnClickListener{
-                val latitude=46.414382
-                val longitude=10.013988
-                val geoLocation = Uri.parse("geo:$latitude,$longitude")
+                val latitude=data[position].coor_lattitude
+                val longitude=data[position].coor_longitude
+                val geoLocation = Uri.parse("google.navigation:q=$latitude,$longitude")
                 val intent = Intent(Intent.ACTION_VIEW,geoLocation)
                 if (intent.resolveActivity(context.packageManager)!=null){
                     context.startActivity(intent)
