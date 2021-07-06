b80d87c3ce1e14810c003a7eb3323d6e50db57bf
HoudaMessaoudi <46978076+HoudaMessaoudi@users.noreply.github.com>
2021-07-06 10:48:18 +0200

    frogs

--- app/src/main/java/com/example/projettdm/retrofit/RetrofitService.kt
+++ app/src/main/java/com/example/projettdm/retrofit/RetrofitService.kt
@@ -7,7 +7,7 @@ object RetrofitService {
 
     val endpoint : Endpoint by lazy {
 
-        Retrofit.Builder().baseUrl("http://1ab586a03542.ngrok.io/")
+        Retrofit.Builder().baseUrl("http://59816cfeba7b.ngrok.io/")
 
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(Endpoint::class.java)
